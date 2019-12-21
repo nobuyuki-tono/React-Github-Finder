3317cee7077c6ae0a5512205bc42c09bd868eb05
Nobu <rockmusic2090@gmail.com>
2019-12-20 21:58:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
+import User from "./components/users/User";
+import Home from "./components/pages/Home";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
+import GithubState from "./context/github/githubState";
+import AlertState from "./context/alert/AlertState";
+
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exsct path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
-}
+};
 
 export default App;
---
+++ src/context/alert/alertContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const alertContecxt = createContext();
+
+export default alertContecxt;
